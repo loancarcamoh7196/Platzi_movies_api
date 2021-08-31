2fc04971fb0caf990d9363869638585500de850e
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-08-31 18:27:03 -0400

    [auth-passport ~ 2021-08-31] Fix, verificacion de funcionamiento scopeHandler- Permisos de usuario

--- utils/middleware/scopesValidationHandler.js
+++ utils/middleware/scopesValidationHandler.js
@@ -6,19 +6,29 @@ function scopesValidationHandler(allowedScopes) {
             // console.log('Missing scopes');
             next(boom.unauthorized('Missing scopes'));
         }
+
+        // console.log("Scopes Permitido:", allowedScopes);
+        // console.log("Scope de Usuario: ", req.user.scopes)
+
         // Mapeo el arreglo de scopes pasados a la ruta y verifico si cada uno de esos elementos se encuentra definido en los scopes del usuario. El resultado va a ser un nuevo arreglo de elementos true y/o false
-    const permisions = allowedScopes.map(scope =>
-        req.user.scopes.includes(scope)
-      );
-      //verifico que no haya elemetos false en el arreglo de permisos (es decir, todos tienen que ser true para pasar al siguiente middleware, con uno que tenga false, significa que todos los permisos no se cumplen y por tanto se le niega el acceso)
-      const hasAccess = !permisions.includes(false);
+        const permisions = allowedScopes.map(scope => req.user.scopes.includes(scope) );
+        // console.log("Permisos concedido: ", permisions);    
+
+        //verifico que no haya elemetos false en el arreglo de permisos (es decir, todos tienen que ser true para pasar al siguiente middleware, con uno que tenga false, significa que todos los permisos no se cumplen y por tanto se le niega el acceso)
+        //   const hasAccess = !permisions.includes(false);
+
+        
+        const hasAccess = allowedScopes
+        .map(allowedScope => req.user.scopes.includes(allowedScope))
+        .find(allowed => Boolean(allowed));
+
+        // console.log('Has Access?: ', hasAccess);
 
         if (hasAccess) {
-            console.log('OK ;)')
+            // console.log('OK ;)')
             next();
         }else {
-            console.log('Insufficient scopes')
-            next(boom.unauthorized('Insufficient scopes'));
+            return next(boom.unauthorized('Insufficient scopes'));
         }
     }
 }
